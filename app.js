24457dbd4ef33f726b9869069eae229096c60152
Olushola Adedokun <sholadedokun@yahoo.com>
2016-10-31 17:53:05 +0100

    trial

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-  config = require('./config/config');
+//  config = require('./config/config');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
