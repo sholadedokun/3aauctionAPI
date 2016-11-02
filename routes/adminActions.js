606eddb719fa1b040ec5c10bd1a3ab82a06538f6
Olushola Adedokun <sholadedokun@yahoo.com>
2016-11-02 12:39:14 +0100

    added Admin inventory view

--- routes/adminActions.js
+++ routes/adminActions.js
@@ -21,7 +21,7 @@ router.get('/inventory', function(req, res, next) {
             populate:{path: 'userId'}
         })
     .populate('biddingSettings')
-    .where('biddingSettings.startTimeReadable').ls(Date.now())
+    .where('biddingSettings.startTimeReadable').lt(Date.now())
     .where('biddingSettings.closeTimeReadable').gt(Date.now())
     .sort({dateCreated:-1})
     .exec (function(err, inventory)
